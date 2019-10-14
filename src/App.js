5d3681a360dcdfdc695f8045271ff0e55573830e
Ubuntu <ubuntu@ip-172-31-38-113.us-east-2.compute.internal>
2019-10-14 23:32:02 +0000

    buttons functioning

--- src/App.js
+++ src/App.js
@@ -14,32 +14,20 @@ class App extends Component {
   
   handleClick = (e) => {
     var guessCount = this.state.attemptsLeft - 1;
-    var buttonIndex = e.target.id;
-    console.log(buttonIndex)
-    var replace = this.state.board
-    if (this.state.resultBoard[buttonIndex] === 'tree') {
-      replace = this.state.board.splice(buttonIndex, 1, this.state.resultBoard[buttonIndex])
-      console.log(replace)
+    if (guessCount >= 0) {
+      var buttonIndex = e.target.id;
+      console.log(buttonIndex)
+      this.state.board.splice(buttonIndex, 1, this.state.resultBoard[buttonIndex])
+      this.setState({board: this.state.board, attemptsLeft: guessCount})
     }
-      
-      
-    this.setState({board: this.state.board})
-    }
+  }
     
-    
-    //this.setState({attemptsLeft:guessCount, resultBoard: newBoard })
-    
-    
-    // if (this.state.board === 0) {
-    //   return 'tree'
-    // }
-    // else if (this.state.board === 1)
   
   
   
   render() {
     return (
-      <div>
+      <div className ="grid">
         <button id="0" onClick={this.handleClick}>{this.state.board[0]} </button>
         <button id ="1" onClick = {this.handleClick}>{this.state.board[1]}</button>
         <button id = "2" onClick = {this.handleClick}>{this.state.board[2]}</button>
@@ -48,7 +36,7 @@ class App extends Component {
         <button id ="5" onClick = {this.handleClick}>{this.state.board[5]}</button>
         <button id = "6" onClick = {this.handleClick}>{this.state.board[6]}</button>
         <button id = "7" onClick = {this.handleClick}>{this.state.board[7]}</button>
-        <button id = "8" onClick = {this.handleClick}>Button 9</button>
+        <button id = "8" onClick = {this.handleClick}>{this.state.board[8]}</button>
         <p>{this.state.attemptsLeft}</p>
       </div>
     );
