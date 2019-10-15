0c0f13079ac4eb7cf3444c06d0cc4ec6da158d8d
Ubuntu <ubuntu@ip-172-31-38-113.us-east-2.compute.internal>
2019-10-15 18:00:48 +0000

    play again button works!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Grid from './Grid.js';
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -32,10 +34,14 @@ class App extends Component {
   startOver = () => {
     //update board state
      //resultBoard with special index defined with  Math.floor(Math.random() * 9)
+    let newBoard = []
+    for (let i = 0; i < this.state.board.length; i++) {
+      newBoard.push('?');
+    }
     
-    let newBoard = [];
+    let newResultBoard = [];
     for (let i = 0; i < this.state.resultBoard.length; i++) {
-      newBoard.push('tree');
+      newResultBoard.push('tree');
     }
    
     let treasureIndex
@@ -45,32 +51,29 @@ class App extends Component {
       bombIndex = Math.floor(Math.random()*9)
     } while (treasureIndex === bombIndex)
     
-    this.state.resultBoard = newBoard
+    
+    this.state.resultBoard = newResultBoard
     this.state.resultBoard.splice(treasureIndex,1,'treasure')
     this.state.resultBoard.splice(bombIndex,1,'bomb')
    
-    this.setState({resultBoard: this.state.resultBoard})
+    this.setState({resultBoard: this.state.resultBoard,
+      board: newBoard, attemptsLeft: 5, won: 'Make a guess!'
+    })
   }
     
   
   render() {
     return (
-      <div className ="grid">
-        <button id="0" onClick={this.handleClick}>{this.state.board[0]} </button>
-        <button id ="1" onClick = {this.handleClick}>{this.state.board[1]}</button>
-        <button id = "2" onClick = {this.handleClick}>{this.state.board[2]}</button>
-        <button id = "3" onClick = {this.handleClick}>{this.state.board[3]}</button>
-        <button id ="4" onClick = {this.handleClick}>{this.state.board[4]}</button>
-        <button id ="5" onClick = {this.handleClick}>{this.state.board[5]}</button>
-        <button id = "6" onClick = {this.handleClick}>{this.state.board[6]}</button>
-        <button id = "7" onClick = {this.handleClick}>{this.state.board[7]}</button>
-        <button id = "8" onClick = {this.handleClick}>{this.state.board[8]}</button>
-        <p>Attempts Left: {this.state.attemptsLeft}</p>
-        <p>{this.state.won}</p>
-        <br/>
-        <button onClick = {this.startOver}>Play Again!</button>
-        
-        
+      <div>
+        <div id="Grid">
+          <Grid boxes={this.handleClick}
+            tries={this.state.attemptsLeft}
+            winner={this.state.won}
+            questionBoard={this.state.board}/>
+        </div>
+        <div className ="PlayAgain">
+          <button onClick = {this.startOver}>Play Again!</button>
+        </div>
       </div>
     );
   }
---
+++ src/Grid.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Grid extends Component {
+    render() {
+    return (
+      <div>
+         <button id="0" onClick={this.props.boxes}>{this.props.questionBoard[0]} </button>
+        <button id ="1" onClick = {this.props.boxes}>{this.props.questionBoard[1]}</button>
+        <button id = "2" onClick = {this.props.boxes}>{this.props.questionBoard[2]}</button>
+        <button id = "3" onClick = {this.props.boxes}>{this.props.questionBoard[3]}</button>
+        <button id ="4" onClick = {this.props.boxes}>{this.props.questionBoard[4]}</button>
+        <button id ="5" onClick = {this.props.boxes}>{this.props.questionBoard[5]}</button>
+        <button id = "6" onClick = {this.props.boxes}>{this.props.questionBoard[6]}</button>
+        <button id = "7" onClick = {this.props.boxes}>{this.props.questionBoard[7]}</button>
+        <button id = "8" onClick = {this.props.boxes}>{this.props.questionBoard[8]}</button>
+        <p>Attempts Left: {this.props.tries}</p>
+        <p>{this.props.winner}</p>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Grid
